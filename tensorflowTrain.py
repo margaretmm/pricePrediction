0264103213df4d4ef63f82dab0d44de988201732
margaretmm <myq927@126.com>
2018-08-29 09:23:23 +0800

    Update and rename tf_example.py to tensorflowTrain.py

--- tensorflowTrain.py
+++ tensorflowTrain.py
@@ -4,18 +4,27 @@ from sklearn.datasets import load_boston
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import scale
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
 boston = load_boston()
-# X = scale(boston.data)
-# y = scale(boston.target.reshape((-1,1)))
-
-
+X = scale(boston.data)
+y = scale(boston.target.reshape((-1,1)))
+len=boston.data.shape[1]
 X_train,X_test,y_train,y_test = train_test_split(boston.data,boston.target,test_size=0.1,random_state=0)
+
+# dfTrain='Data_washed_all2.csv'
+# keep_prob=1
+# df = pd.read_csv(dfTrain,header=0)
+# arr=["Rooms","Area","Decorate","subway","FiveYear","Toward_s","Toward_e","Toward_w","Floor_h","Floor_m","Floor_l","BuyYesrs<3","BuyYesrs_3_5","BuyYesrs_6_10","BuyYesrs>10","ProductHouse","area_bj","area_gs","area_yh","area_xh","area_xc","area_xs"]
+# len=len(arr)
+# df_train=df[arr].values
+# df_target=df["Price"].values#.values.reshape(len(df),1)
+# X_train,X_test,y_train,y_test = train_test_split(df_train,df_target,test_size=0.1,random_state=0)
+
 X_train = scale(X_train)
 X_test = scale(X_test)
 y_train = scale(y_train.reshape((-1,1)))
 y_test = scale(y_test.reshape((-1,1)))
-
 
 def add_layer(inputs,input_size,output_size,activation_function=None):
     with tf.variable_scope("Weights"):
@@ -37,21 +46,29 @@ ys = tf.placeholder(shape=[None,1],dtype=tf.float32,name="y_true")
 keep_prob_s = tf.placeholder(dtype=tf.float32)
 
 with tf.name_scope("layer_1"):
-    l1 = add_layer(xs,13,10,activation_function=tf.nn.relu)
+    l1 = add_layer(xs,len,10,activation_function=tf.nn.relu)
 # with tf.name_scope("layer_2"):
-#     l2 = add_layer(l1,6,10,activation_function=tf.nn.relu)
+#     l2 = add_layer(l1,10,10,activation_function=tf.nn.relu)
 with tf.name_scope("y_pred"):
     pred = add_layer(l1,10,1)
 
 # 这里多于的操作，是为了保存pred的操作，做恢复用。我只知道这个笨方法。
 pred = tf.add(pred,0,name='pred')
 
+initial_learning_rate = 0.001
+global_step = tf.Variable(0, trainable=False)
+decay_steps = 200
+decay_rate = 0.98
 with tf.name_scope("loss"):
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - pred),reduction_indices=[1]))  # mse
     tf.summary.scalar("loss",tensor=loss)
 with tf.name_scope("train"):
-    # train_op =tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
-    train_op = tf.train.AdamOptimizer(learning_rate=0.01).minimize(loss)
+    # train_op =tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss,global_step = global_step)
+    #total_loss = slim.losses.get_total_loss()
+    learning_rate = tf.train.exponential_decay(initial_learning_rate, global_step, decay_steps, decay_rate, True,
+                                               name='learning_rate')
+    # Passing global_step to minimize() will increment it at each step.
+    train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss,global_step = global_step)
 
 # draw pics
 fig = plt.figure()
@@ -62,8 +79,8 @@ plt.ion()
 plt.show()
 
 # parameters
-keep_prob=1  # 防止过拟合，取值一般在0.5到0.8。我这里是1，没有做过拟合处理
-ITER =5000  # 训练次数
+keep_prob=0.8  # 防止过拟合，取值一般在0.5到0.8。我这里是1，没有做过拟合处理
+ITER =30000  # 训练次数
 
 
 def fit(X, y, ax, n, keep_prob):
@@ -76,8 +93,8 @@ def fit(X, y, ax, n, keep_prob):
         sess.run(init)
         for i in range(n):
             _loss, _ = sess.run([loss, train_op], feed_dict=feed_dict_train)
-
             if i % 100 == 0:
+                print(sess.run([learning_rate]))
                 print("epoch:%d\tloss:%.5f" % (i, _loss))
                 y_pred = sess.run(pred, feed_dict=feed_dict_train)
                 rs = sess.run(merged, feed_dict=feed_dict_train)
